1102651aa7294e62ca41670fb6f3553dbd3dc6ea
富山悟志 <>
2024-12-23 22:58:31 +0900

    add personal service in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersonalModule } from './personal/personal.module';
 import { PersonalController } from './personal/personal.controller';
+import { PersonalService } from './personal/personal.service';
 
 @Module({
   imports: [PersonalModule],
-  controllers: [AppController],
-  providers: [AppService, PersonalController],
+  controllers: [
+    AppController,
+    PersonalController
+  ],
+  providers: [
+    AppService,
+    PersonalService,
+  ],
 })
 export class AppModule { }
